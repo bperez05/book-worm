cb48acf81b810e46f989a9698f79e16da752bdcf
bperez05 <perez.brandon55@yahoo.com>
2022-03-15 00:03:06 -0500

    added buildpack and edited server.js

--- server/server.js
+++ server/server.js
@@ -1,36 +1,36 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
-const {ApolloServer} = require('apollo-server-express');
-const {typeDefs, resolvers} = require('./schemas');
-const {authMiddeleware} = require('./utils/auth');
-
-const app = express();
-const PORT = process.env.PORT ||3001;
-
+// const routes = require('./routes');
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
-  context: authMiddeleware
+  resolvers
 });
+const app = express();
+const PORT = process.env.PORT || 80;
 
-server.applyMiddleware({app});
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
+  console.log('In Production');
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// app.use(routes); //take this out later
+
+server.start().then(res => {
+  server.applyMiddleware({ app })//Add apollo-server to mididleware
 });
 
-app.use(routes);
-
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
